b2bde22ce8f36a0221f533eaeba103c04ad6e734
khouloud <ggirlk2@gmail.com>
2020-03-23 22:15:08 +0100

    puppet create file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
-class createfile {
-  file { '/tmp/holberton':
-    ensure  => '/tmp/holberton',
-    replace => 'yes',
-    content => 'I love Puppet',
-    mode => '0744',
-    group => 'www-data',
-    owner => 'www-data',
-  }
+
+file { '/tmp/holberton':
+  ensure  => '/tmp/holberton',
+  replace => 'yes',
+  content => 'I love Puppet',
+  mode => '0744',
+  group => 'www-data',
+  owner => 'www-data',
 }
