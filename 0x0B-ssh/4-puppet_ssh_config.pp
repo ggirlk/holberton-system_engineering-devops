a1ba50dbf0d00f818fa9122bf509e5a4f8cb453f
ggirlk <ggirlk2@gmail.com>
2020-03-26 20:14:59 +0000

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -2,9 +2,11 @@
 exec { 'ssh1':
   command => 'echo IdentityFile ~/.ssh/holberton >> /etc/ssh/ssh_config',
   path    => '/usr/bin/:/bin/',
+  provider => shell,
 }
 
 exec { 'ssh2':
-  command => 'echo PasswordAuthentification no >> /etc/ssh/ssh_config',
-  path    => '/usr/bin/:/bin/',
+  command  => 'echo PasswordAuthentification no >> /etc/ssh/ssh_config',
+  path     => '/usr/bin/:/bin/',
+  provider => shell,
 }
