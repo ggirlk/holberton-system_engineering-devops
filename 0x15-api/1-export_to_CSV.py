30b9444bb5f5747ff8cc401eb53eaea177bb53d2
ggirlk <ggirlk2@gmail.com>
2020-05-04 20:52:33 +0000

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -5,11 +5,12 @@ import sys
 
 
 
-p = {"userId" : sys.argv[1]}
-p2 = {"id" : sys.argv[1]}
-r = requests.get('https://jsonplaceholder.typicode.com/todos', params=p).json()
-user = requests.get('https://jsonplaceholder.typicode.com/users', params=p2).json()[0]
-with open(sys.argv[1]+'.csv', 'a+') as f:
-    for i in r:
-        f.write('"{}","{}","{}","{}"\n'
-                .format(i["userId"], user['name'], i["completed"], i["title"]))
+if __name__ == '__main__':
+    p = {"userId" : sys.argv[1]}
+    p2 = {"id" : sys.argv[1]}
+    r = requests.get('https://jsonplaceholder.typicode.com/todos', params=p).json()
+    user = requests.get('https://jsonplaceholder.typicode.com/users', params=p2).json()[0]
+    with open(sys.argv[1]+'.csv', 'a+') as f:
+        for i in r:
+            f.write('"{}","{}","{}","{}"\n'
+                    .format(i["userId"], user['name'], i["completed"], i["title"]))
