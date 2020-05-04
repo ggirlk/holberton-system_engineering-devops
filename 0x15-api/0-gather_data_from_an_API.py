54d540f92d02f53be359d3a5b8ca9a6ca2cc4b2d
ggirlk <ggirlk2@gmail.com>
2020-05-04 20:49:51 +0000

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,16 +5,17 @@ import sys
 
 
 
-p = {"userId" : sys.argv[1]}
-p2 = {"id" : sys.argv[1]}
-r = requests.get('https://jsonplaceholder.typicode.com/todos', params=p).json()
-done = 0
-for i in r:
-    if i['completed'] == True:
-        done += 1
-user = requests.get('https://jsonplaceholder.typicode.com/users', params=p2).json()[0]
-print("Employee {} is done with tasks({}/{}):"
-      .format(user['name'], done, len(r)))
-for i in r:
-    if i['completed'] == True:
-        print('\t{}'.format(i['title']))
+if __name__ == '__main__':
+    p = {"userId" : sys.argv[1]}
+    p2 = {"id" : sys.argv[1]}
+    r = requests.get('https://jsonplaceholder.typicode.com/todos', params=p).json()
+    done = 0
+    for i in r:
+        if i['completed'] == True:
+            done += 1
+    user = requests.get('https://jsonplaceholder.typicode.com/users', params=p2).json()[0]
+    print("Employee {} is done with tasks({}/{}):"
+          .format(user['name'], done, len(r)))
+    for i in r:
+        if i['completed'] == True:
+            print('\t{}'.format(i['title']))
