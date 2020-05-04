30ec1ab834a70f908af329f1adfb1cce84834169
ggirlk <ggirlk2@gmail.com>
2020-05-04 21:28:51 +0000

    Export to JSON

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -2,14 +2,22 @@
 """ Export to JSON """
 import requests
 import sys
+import json
 
 
 
-p = {"userId" : sys.argv[1]}
-p2 = {"id" : sys.argv[1]}
-r = requests.get('https://jsonplaceholder.typicode.com/todos', params=p).json()
-user = requests.get('https://jsonplaceholder.typicode.com/users', params=p2).json()[0]
-with open(sys.argv[1]+'.csv', 'a+') as f:
+if __name__ == '__main__':
+    p = {"userId" : sys.argv[1]}
+    p2 = {"id" : sys.argv[1]}
+    r = requests.get('https://jsonplaceholder.typicode.com/todos', params=p).json()
+    user = requests.get('https://jsonplaceholder.typicode.com/users', params=p2).json()[0]
+    jdata = {}
+    jdata[user['id']] = []
     for i in r:
-        f.write('"{}","{}","{}","{}"\n'
-                .format(i["userId"], user['name'], i["completed"], i["title"]))
+        jdata[user['id']].append({
+             "task": i["title"],
+             "completed": i["completed"],
+             "username": user["name"]
+        })
+    with open(sys.argv[1]+'.json', 'a+') as f:
+        json.dump(jdata, f)
