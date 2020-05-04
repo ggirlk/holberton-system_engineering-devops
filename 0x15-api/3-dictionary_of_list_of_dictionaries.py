31ead98070c27b7e2747cf30b344dbdcabf5bdd7
ggirlk <ggirlk2@gmail.com>
2020-05-04 22:16:51 +0000

    Dictionary of list of dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -8,15 +8,17 @@ import json
 if __name__ == '__main__':
     user = requests.get('https://jsonplaceholder.typicode.com/users').json()
     jdata = {}
+    data = {}
     for u in user:
         p = {"userId" : u['id']}
         r = requests.get('https://jsonplaceholder.typicode.com/todos', params=p).json()
-        jdata[u['id']] = []
+        jdata = []
         for i in r:
-            jdata[u['id']].append({
+            jdata.append({
                  "username": u["name"],
                  "task": i["title"],
                  "completed": i["completed"]
             })
+        data[u['id']] = jdata
     with open('todo_all_employees.json', 'a+') as f:
-        json.dump(jdata, f)
+        json.dump(data, f)
